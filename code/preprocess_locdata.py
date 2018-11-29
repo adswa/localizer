ec595564cfb3ced43203efc220798a1f2fa47547
Adina Wagner <adina.wagner@t-online.de>
2018-11-29 17:58:34 -0500

    only debugging

--- code/preprocess_locdata.py
+++ code/preprocess_locdata.py
@@ -1,4 +1,4 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 """
 =====================================================================================
@@ -527,6 +527,9 @@ highpass_operand = lambda x: '-bptf %.10f -1' % x
 
 
 
+
+
+
 def create_susan_smooth(name="susan_smooth", separate_masks=True):
     """Create a SUSAN smoothing workflow
 
@@ -675,6 +678,156 @@ def create_susan_smooth(name="susan_smooth", separate_masks=True):
 
     return susan_smooth
 
+
+#
+#def create_susan_smooth(name="susan_smooth", separate_masks=True):
+#    """Create a SUSAN smoothing workflow
+#
+#    Parameters
+#    ----------
+#
+#    ::
+#
+#        name : name of workflow (default: susan_smooth)
+#        separate_masks : separate masks for each run
+#
+#    Inputs::
+#
+#        inputnode.in_files : functional runs (filename or list of filenames)
+#        inputnode.fwhm : fwhm for smoothing with SUSAN (float or list of floats)
+#        inputnode.mask_file : mask used for estimating SUSAN thresholds (but not for smoothing)
+#
+#    Outputs::
+#
+#        outputnode.smoothed_files : functional runs (filename or list of filenames)
+#
+#    Example
+#    -------
+#
+#    >>> smooth = create_susan_smooth()
+#    >>> smooth.inputs.inputnode.in_files = 'f3.nii'
+#    >>> smooth.inputs.inputnode.fwhm = 5
+#    >>> smooth.inputs.inputnode.mask_file = 'mask.nii'
+#    >>> smooth.run() # doctest: +SKIP
+#
+#    """
+#
+#    # replaces the functionality of a "for loop"
+#    def cartesian_product(fwhms, in_files, usans, btthresh):
+#        from nipype.utils.filemanip import ensure_list
+#        # ensure all inputs are lists
+#        in_files = ensure_list(in_files)
+#        fwhms = [fwhms] if isinstance(fwhms, (int, float)) else fwhms
+#        # create cartesian product lists (s_<name> = single element of list)
+#        cart_in_file = [
+#            s_in_file for s_in_file in in_files for s_fwhm in fwhms
+#        ]
+#        cart_fwhm = [s_fwhm for s_in_file in in_files for s_fwhm in fwhms]
+#        cart_usans = [s_usans for s_usans in usans for s_fwhm in fwhms]
+#        cart_btthresh = [
+#            s_btthresh for s_btthresh in btthresh for s_fwhm in fwhms
+#        ]
+#
+#        return cart_in_file, cart_fwhm, cart_usans, cart_btthresh
+#
+#    susan_smooth = pe.Workflow(name=name)
+#    """
+#    Set up a node to define all inputs required for the preprocessing workflow
+#
+#    """
+#
+#    inputnode = pe.Node(
+#        interface=util.IdentityInterface(
+#            fields=['in_files', 'fwhm', 'mask_file']),
+#        name='inputnode')
+#    """
+#    Smooth each run using SUSAN with the brightness threshold set to 75%
+#    of the median value for each run and a mask consituting the mean
+#    functional
+#    """
+##    import pdb; pdb.set_trace()
+#    multi_inputs = pe.Node(
+#        util.Function(
+#            function=cartesian_product,
+#            output_names=[
+#                'cart_in_file', 'cart_fwhm', 'cart_usans', 'cart_btthresh'
+#            ]),
+#        name='multi_inputs')
+#
+#    smooth = pe.MapNode(
+#        interface=fsl.SUSAN(),
+#        iterfield=['in_file', 'brightness_threshold', 'usans', 'fwhm'],
+#        name='smooth')
+#    """
+#    Determine the median value of the functional runs using the mask
+#    """
+#
+#    if separate_masks:
+#        median = pe.MapNode(
+#            interface=fsl.ImageStats(op_string='-k %s -p 50'),
+#            iterfield=['in_file', 'mask_file'],
+#            name='median')
+#    else:
+#        median = pe.MapNode(
+#            interface=fsl.ImageStats(op_string='-k %s -p 50'),
+#            iterfield=['in_file'],
+#            name='median')
+#    susan_smooth.connect(inputnode, 'in_files', median, 'in_file')
+#    susan_smooth.connect(inputnode, 'mask_file', median, 'mask_file')
+#    """
+#    Mask the motion corrected functional runs with the dilated mask
+#    """
+#
+#    if separate_masks:
+#        mask = pe.MapNode(
+#            interface=fsl.ImageMaths(suffix='_mask', op_string='-mas'),
+#            iterfield=['in_file', 'in_file2'],
+#            name='mask')
+#    else:
+#        mask = pe.MapNode(
+#            interface=fsl.ImageMaths(suffix='_mask', op_string='-mas'),
+#            iterfield=['in_file'],
+#            name='mask')
+#    susan_smooth.connect(inputnode, 'in_files', mask, 'in_file')
+#    susan_smooth.connect(inputnode, 'mask_file', mask, 'in_file2')
+#    """
+#    Determine the mean image from each functional run
+#    """
+#
+#    meanfunc = pe.MapNode(
+#        interface=fsl.ImageMaths(op_string='-Tmean', suffix='_mean'),
+#        iterfield=['in_file'],
+#        name='meanfunc2')
+#    susan_smooth.connect(mask, 'out_file', meanfunc, 'in_file')
+#    """
+#    Merge the median values with the mean functional images into a coupled list
+#    """
+#
+#    merge = pe.Node(interface=util.Merge(2, axis='hstack'), name='merge')
+#    susan_smooth.connect(meanfunc, 'out_file', merge, 'in1')
+#    susan_smooth.connect(median, 'out_stat', merge, 'in2')
+#    """
+#    Define a function to get the brightness threshold for SUSAN
+#    """
+#
+#    susan_smooth.connect([
+#        (inputnode, multi_inputs, [('in_files', 'in_files'), ('fwhm',
+#                                                              'fwhms')]),
+#        (median, multi_inputs, [(('out_stat', getbtthresh), 'btthresh')]),
+#        (merge, multi_inputs, [(('out', getusans), 'usans')]),
+#        (multi_inputs, smooth,
+#         [('cart_in_file', 'in_file'), ('cart_fwhm', 'fwhm'),
+#          ('cart_btthresh', 'brightness_threshold'), ('cart_usans', 'usans')]),
+#    ])
+#
+#    outputnode = pe.Node(
+#        interface=util.IdentityInterface(fields=['smoothed_files']),
+#        name='outputnode')
+#
+#    susan_smooth.connect(smooth, 'smoothed_file', outputnode, 'smoothed_files')
+#
+#    return susan_smooth
+#
 
 def preprocess_loc(experiment_dir,
                     subject_id = None,
@@ -856,7 +1009,7 @@ if __name__ == '__main__':
                             session_id=args.session_id,
                             task_id=args.task_id)
     pre.base_dir=work_dir
-    pre.run(plugin='MultiProc', plugin_args={'n_procs':7})
+    #pre.run(plugin='MultiProc', plugin_args={'n_procs':7})
 
     if args.preproc==False:
         firstlevel = runfirst_forrest(json_desc=json_desc,
