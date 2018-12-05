ce62cca13fe9bf4e0caba004db4c57737fa342dd
Adina Wagner <adina.wagner@t-online.de>
2018-12-05 11:31:54 -0500

    adapt script to use aligned visloc data instead of raw

--- code/preprocess_locdata.py
+++ code/preprocess_locdata.py
@@ -679,156 +679,6 @@ def create_susan_smooth(name="susan_smooth", separate_masks=True):
     return susan_smooth
 
 
-#
-#def create_susan_smooth(name="susan_smooth", separate_masks=True):
-#    """Create a SUSAN smoothing workflow
-#
-#    Parameters
-#    ----------
-#
-#    ::
-#
-#        name : name of workflow (default: susan_smooth)
-#        separate_masks : separate masks for each run
-#
-#    Inputs::
-#
-#        inputnode.in_files : functional runs (filename or list of filenames)
-#        inputnode.fwhm : fwhm for smoothing with SUSAN (float or list of floats)
-#        inputnode.mask_file : mask used for estimating SUSAN thresholds (but not for smoothing)
-#
-#    Outputs::
-#
-#        outputnode.smoothed_files : functional runs (filename or list of filenames)
-#
-#    Example
-#    -------
-#
-#    >>> smooth = create_susan_smooth()
-#    >>> smooth.inputs.inputnode.in_files = 'f3.nii'
-#    >>> smooth.inputs.inputnode.fwhm = 5
-#    >>> smooth.inputs.inputnode.mask_file = 'mask.nii'
-#    >>> smooth.run() # doctest: +SKIP
-#
-#    """
-#
-#    # replaces the functionality of a "for loop"
-#    def cartesian_product(fwhms, in_files, usans, btthresh):
-#        from nipype.utils.filemanip import ensure_list
-#        # ensure all inputs are lists
-#        in_files = ensure_list(in_files)
-#        fwhms = [fwhms] if isinstance(fwhms, (int, float)) else fwhms
-#        # create cartesian product lists (s_<name> = single element of list)
-#        cart_in_file = [
-#            s_in_file for s_in_file in in_files for s_fwhm in fwhms
-#        ]
-#        cart_fwhm = [s_fwhm for s_in_file in in_files for s_fwhm in fwhms]
-#        cart_usans = [s_usans for s_usans in usans for s_fwhm in fwhms]
-#        cart_btthresh = [
-#            s_btthresh for s_btthresh in btthresh for s_fwhm in fwhms
-#        ]
-#
-#        return cart_in_file, cart_fwhm, cart_usans, cart_btthresh
-#
-#    susan_smooth = pe.Workflow(name=name)
-#    """
-#    Set up a node to define all inputs required for the preprocessing workflow
-#
-#    """
-#
-#    inputnode = pe.Node(
-#        interface=util.IdentityInterface(
-#            fields=['in_files', 'fwhm', 'mask_file']),
-#        name='inputnode')
-#    """
-#    Smooth each run using SUSAN with the brightness threshold set to 75%
-#    of the median value for each run and a mask consituting the mean
-#    functional
-#    """
-##    import pdb; pdb.set_trace()
-#    multi_inputs = pe.Node(
-#        util.Function(
-#            function=cartesian_product,
-#            output_names=[
-#                'cart_in_file', 'cart_fwhm', 'cart_usans', 'cart_btthresh'
-#            ]),
-#        name='multi_inputs')
-#
-#    smooth = pe.MapNode(
-#        interface=fsl.SUSAN(),
-#        iterfield=['in_file', 'brightness_threshold', 'usans', 'fwhm'],
-#        name='smooth')
-#    """
-#    Determine the median value of the functional runs using the mask
-#    """
-#
-#    if separate_masks:
-#        median = pe.MapNode(
-#            interface=fsl.ImageStats(op_string='-k %s -p 50'),
-#            iterfield=['in_file', 'mask_file'],
-#            name='median')
-#    else:
-#        median = pe.MapNode(
-#            interface=fsl.ImageStats(op_string='-k %s -p 50'),
-#            iterfield=['in_file'],
-#            name='median')
-#    susan_smooth.connect(inputnode, 'in_files', median, 'in_file')
-#    susan_smooth.connect(inputnode, 'mask_file', median, 'mask_file')
-#    """
-#    Mask the motion corrected functional runs with the dilated mask
-#    """
-#
-#    if separate_masks:
-#        mask = pe.MapNode(
-#            interface=fsl.ImageMaths(suffix='_mask', op_string='-mas'),
-#            iterfield=['in_file', 'in_file2'],
-#            name='mask')
-#    else:
-#        mask = pe.MapNode(
-#            interface=fsl.ImageMaths(suffix='_mask', op_string='-mas'),
-#            iterfield=['in_file'],
-#            name='mask')
-#    susan_smooth.connect(inputnode, 'in_files', mask, 'in_file')
-#    susan_smooth.connect(inputnode, 'mask_file', mask, 'in_file2')
-#    """
-#    Determine the mean image from each functional run
-#    """
-#
-#    meanfunc = pe.MapNode(
-#        interface=fsl.ImageMaths(op_string='-Tmean', suffix='_mean'),
-#        iterfield=['in_file'],
-#        name='meanfunc2')
-#    susan_smooth.connect(mask, 'out_file', meanfunc, 'in_file')
-#    """
-#    Merge the median values with the mean functional images into a coupled list
-#    """
-#
-#    merge = pe.Node(interface=util.Merge(2, axis='hstack'), name='merge')
-#    susan_smooth.connect(meanfunc, 'out_file', merge, 'in1')
-#    susan_smooth.connect(median, 'out_stat', merge, 'in2')
-#    """
-#    Define a function to get the brightness threshold for SUSAN
-#    """
-#
-#    susan_smooth.connect([
-#        (inputnode, multi_inputs, [('in_files', 'in_files'), ('fwhm',
-#                                                              'fwhms')]),
-#        (median, multi_inputs, [(('out_stat', getbtthresh), 'btthresh')]),
-#        (merge, multi_inputs, [(('out', getusans), 'usans')]),
-#        (multi_inputs, smooth,
-#         [('cart_in_file', 'in_file'), ('cart_fwhm', 'fwhm'),
-#          ('cart_btthresh', 'brightness_threshold'), ('cart_usans', 'usans')]),
-#    ])
-#
-#    outputnode = pe.Node(
-#        interface=util.IdentityInterface(fields=['smoothed_files']),
-#        name='outputnode')
-#
-#    susan_smooth.connect(smooth, 'smoothed_file', outputnode, 'smoothed_files')
-#
-#    return susan_smooth
-#
-
 def preprocess_loc(experiment_dir,
                     subject_id = None,
                     run_id = None,
@@ -883,8 +733,7 @@ def preprocess_loc(experiment_dir,
 
 
     templates = {#'anat':'inputs/tnt/{subject_id}/t1w/brain.nii.gz',
-                 'func':'studyforrest-data-visualrois/src/phase2/{subject_id}/{session_id}/func/sub-*_ses-localizer_task-objectcategories_{run_id}_bold.nii.gz'}
-#here: studyforrest-data-visualrois/src/phase2
+                 'func':'sourcedata/aligned/{subject_id}/in_bold3Tp2/sub-*_task-objectcategories_{run_id}_bold.nii.gz'}
 
     sf = pe.Node(SelectFiles(templates),
               name='selectfiles')
@@ -907,10 +756,10 @@ def preprocess_loc(experiment_dir,
         subs.append(('_dilatemask0', ''))
         subs.append(('_maskfunc30', ''))
         subs.append(('_meanfunc30', ''))
-        subs.append(('bold_st_dtype_bet_thresh_dil', 'type-brain_mask'))
-        subs.append(('bold_st_dtype_mask_smooth_mask_gms', 'desc-mean'))
-        subs.append(('bold_st_dtype_mask_smooth_mask', 'desc-smooth'))
-        subs.append(('bold_st_dtype_mask_smooth_mask gms_tempfilt_maths', 'desc-highpass_bold'))
+        subs.append(('bold_st_dtype_bet_thresh_dil', 'space-custom-subject_type-brain_mask'))
+        subs.append(('bold_st_dtype_mask_smooth_mask_gms', 'space-custom-subject_desc-mean'))
+        subs.append(('bold_st_dtype_mask_smooth_mask', 'space-custom-subject_desc-smooth'))
+        subs.append(('bold_st_dtype_mask_smooth_mask gms_tempfilt_maths', 'space-custom-subject_desc-highpass_bold'))
         subs.append(('_mean', ''))
         subs.append(('mean_tempfilt_maths','highpass_bold'))
         return subs
